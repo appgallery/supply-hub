1ce4ef57312a288caefbb4e5d3ffe9a844f289bb
Shifa <shifa.appgallery@gmail.com>
2026-07-28 12:05:30 +0530

    changes:

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -11,7 +11,7 @@ export const login = async (
 
         return res.status(200).json({
             status:true,
-            message:"Login successfully..."
+            message:"Login successfully...",
             ...result
             
         });
